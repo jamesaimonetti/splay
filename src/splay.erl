a7c74b7f9458498373383503ce3942510aa28265
James Aimonetti <james.aimonetti@gmail.com>
2010-04-08 16:01:52 -0400

    small changes

--- src/splay.erl
+++ src/splay.erl
@@ -1,7 +1,8 @@
 -module(splay).
 
--export([new/0, size/1, to_list/1, from_list/1]).
--export([add/2, find_min/1, remove/2, rm_min/1]).
+-export([new/0, size/1, height/1, to_list/1, from_list/1]).
+-export([add/2, find_min/1, has/2, splay/2]).
+-export([merge/2, delete/2, delete_min/1]).
 
 -record(splay, {
           lte={},
@@ -16,6 +17,17 @@ new() -> empty.
 size(empty) -> 0;
 size(#splay{lte=A, gt=B}) -> splay:size(A) + 1 + splay:size(B).
 
+%% height(Splay) -> int()
+%%   returns the height of the tree
+height(empty) -> 0;
+height(#splay{lte=A, gt=B}) ->
+    Ha = height(A),
+    Hb = height(B),
+    case Ha > Hb of
+        true -> 1 + Ha;
+        false -> 1 + Hb
+    end.
+
 %% to_list(Splay) -> [Element].
 to_list(empty) -> [];
 to_list(#splay{lte=A, data=X, gt=B}) ->
@@ -29,27 +41,41 @@ add(X, T) ->
     {Lte, Gt} = partition(X, T),
     #splay{lte=Lte, data=X, gt=Gt}.
 
-%% find_min(Splay) -> {Splay, Min}
-find_min(#splay{lte=empty, data=Min}=T) ->
-    {Lte, Gt} = partition(Min, T),
-    {#splay{lte=Lte, data=Min, gt=Gt}, Min};
-find_min(#splay{lte=A}) -> find_min(A).
+%% has(Element, Splay) -> {Splay, bool()}
+has(X, T) ->
+    Has = has_aux(X, T),
+    {splay(X, T, Has), Has}.
 
-%% remove(Element, Splay) -> Splay
-remove(_, empty) -> empty;
-remove(X, #splay{lte=A, data=Y}=T) when X < Y ->
-    T#splay{lte=remove(X, A)};
-remove(X, #splay{data=Y, gt=B}=T) when X > Y ->
-    T#splay{gt=remove(X, B)};
-remove(X, #splay{lte=A, data=X, gt=B}) ->
+has_aux(_, empty) -> false;
+has_aux(X, #splay{lte=A, data=Y}) when X < Y ->
+    has_aux(X, A);
+has_aux(X, #splay{data=Y, gt=B}) when X > Y ->
+    has_aux(X, B);
+has_aux(X, #splay{data=X}) -> true.
+
+%% find_min(Splay) -> {Splay, Min}
+find_min(T) ->
+    Min = find_min_aux(T),
+    {splay(Min, T), Min}.
+
+find_min_aux(#splay{lte=empty, data=Min}) -> Min;
+find_min_aux(#splay{lte=A}) -> find_min_aux(A).
+
+%% delete(Element, Splay) -> Splay
+delete(_, empty) -> empty;
+delete(X, #splay{lte=A, data=Y}=T) when X < Y ->
+    T#splay{lte=delete(X, A)};
+delete(X, #splay{data=Y, gt=B}=T) when X > Y ->
+    T#splay{gt=delete(X, B)};
+delete(X, #splay{lte=A, data=X, gt=B}) ->
     merge(A, B).
 
-%% rm_min(Splay) -> {Splay, Element}.
-rm_min(#splay{lte=empty, gt=B}) -> {B, undefined};
-rm_min(#splay{lte=#splay{lte=empty, gt=B}, data=Y, gt=C}) ->
+%% delete_min(Splay) -> {Splay, Element}.
+delete_min(#splay{lte=empty, gt=B}) -> {B, undefined};
+delete_min(#splay{lte=#splay{lte=empty, gt=B}, data=Y, gt=C}) ->
     {#splay{lte=B, data=Y, gt=C}, Y};
-rm_min(#splay{lte=#splay{lte=A, data=X, gt=B}, data=Y, gt=C}) ->
-    {Lte, Min} = rm_min(A),
+delete_min(#splay{lte=#splay{lte=A, data=X, gt=B}, data=Y, gt=C}) ->
+    {Lte, Min} = delete_min(A),
     {#splay{lte=Lte, data=X, gt=#splay{lte=B, data=Y, gt=C}}, Min}.
 
 merge(empty, T) -> T;
@@ -57,35 +83,46 @@ merge(#splay{lte=A, data=X, gt=B}, T) ->
     {TA, TB} = partition(X, T),
     #splay{lte=merge(TA, A), data=X, gt=merge(TB, B)}.
 
+%% splay(Element, Splay) -> Splay
+%%   If element exists, Moves node with Element to root of splay; otherwise just re-arranges the tree
+splay(X, T) ->
+    splay(X, T, has_aux(X, T)).
+
+%% short-circuit splay/2 when has_aux() is known
+splay(X, T, true) ->
+    {Lte, Gt} = partition(X, T),
+    #splay{lte=delete(X, Lte), data=X, gt=Gt};
+splay(X, T, false) ->
+    {Lte, Gt} = partition(X, T),
+    merge(Lte, Gt).
+
+%% partition isn't concerned if there's duplicate data in the tree
 partition(_Pivot, empty) -> {empty,empty};
+partition(Pivot, #splay{lte=A, data=X, gt=B}=T) when X =< Pivot ->
+    case B of
+        empty ->
+            {T, B};
+        #splay{lte=B1, data=Y, gt=B2} ->
+            case Y =< Pivot of
+                true ->
+                    {Lte, Gt} = partition(Pivot, B2),
+                    {#splay{lte=#splay{lte=A, data=X, gt=B1}, data=Y, gt=Lte}, Gt};
+                false ->
+                    {Lte, Gt} = partition(Pivot, B1),
+                    {#splay{lte=A, data=X, gt=Lte}, #splay{lte=Gt, data=Y, gt=B2}}
+            end
+    end;
 partition(Pivot, #splay{lte=A, data=X, gt=B}=T) ->
-    case X =< Pivot of
-        true ->
-            case B of
-                empty ->
-                    {T, B};
-                #splay{lte=B1, data=Y, gt=B2} ->
-                    case Y =< Pivot of
-                        true ->
-                            {Lte, Gt} = partition(Pivot, B2),
-                            {#splay{lte=#splay{lte=A, data=X, gt=B1}, data=Y, gt=Lte}, Gt};
-                        false ->
-                            {Lte, Gt} = partition(Pivot, B1),
-                            {#splay{lte=A, data=X, gt=Lte}, #splay{lte=Gt, data=Y, gt=B2}}
-                    end
-            end;
-        false ->
-            case A of
-                empty ->
-                    {A, T};
-                #splay{lte=A1, data=Y, gt=A2} ->
-                    case Y =< Pivot of
-                        true ->
-                            {Lte, Gt} = partition(Pivot, A2),
-                            {#splay{lte=A1, data=Y, gt=Lte}, #splay{lte=Gt, data=X, gt=B}};
-                        false ->
-                            {Lte, Gt} = partition(Pivot, A1),
-                            {Lte, #splay{lte=Gt, data=Y, gt=#splay{lte=A2, data=X, gt=B}}}
-                    end
+    case A of
+        empty ->
+            {A, T};
+        #splay{lte=A1, data=Y, gt=A2} ->
+            case Y =< Pivot of
+                true ->
+                    {Lte, Gt} = partition(Pivot, A2),
+                    {#splay{lte=A1, data=Y, gt=Lte}, #splay{lte=Gt, data=X, gt=B}};
+                false ->
+                    {Lte, Gt} = partition(Pivot, A1),
+                    {Lte, #splay{lte=Gt, data=Y, gt=#splay{lte=A2, data=X, gt=B}}}
             end
     end.
--- src/splaytest.erl
+++ src/splaytest.erl
@@ -81,5 +81,60 @@ test_dict_fetch([{K,V}|Kvs], D) ->
         E -> erlang:error({missing_pair, K, V, found, E})
     end.
 
+test_splay() ->
+    Els = random_els(10000),
+    {_,_} = statistics(runtime),
+    Splay0 = test_splay_add(Els, splay:new()),
+    {_,AddTime} = statistics(runtime),
+    io:format("From ~p els, Splay0 has ~p els~n", [ length(Els), splaysets:size(Splay0)]),
+    show_splay_height(Splay0, splay:size(Splay0)),
+    {ok, Splay01} = test_splay_random_fetch(Els, Splay0, 1000),
+    show_splay_height(Splay01, splay:size(Splay01)),
+    {ok, Splay02} = test_splay_random_fetch(Els, Splay01, 1000),
+    show_splay_height(Splay02, splay:size(Splay02)),
+    {ok, Splay03} = test_splay_random_fetch(Els, Splay02, 1000),
+    show_splay_height(Splay03, splay:size(Splay03)),
+    {_,_} = statistics(runtime),
+    {Splay04, Min} = splay:find_min(Splay03),
+    io:format("Min: ~p~n", [Min]),
+    show_splay_height(Splay03, splay:size(Splay04)),
+    {ok, Splay1} = test_splay_fetch(Els, Splay04),
+    show_splay_height(Splay1, splay:size(Splay1)),
+    io:format("Test Fetch returned ok ~n", []),
+    {_,FetchTime} = statistics(runtime),
+    Splay2 = test_splay_remove(Els, Splay1),
+    {_,RemoveTime} = statistics(runtime),
+    io:format("Removed from splay. Splay now ~p~n", [Splay2]),
+    io:format("Add took ~p ms. Fetching took ~p ms. Removal took ~p ms.~n", [AddTime, FetchTime, RemoveTime]),
+    ok.
+
+test_splay_add([], Splay) -> show_splay_height(Splay, splay:size(Splay)), Splay;
+test_splay_add([E|Es], Splay) ->
+    test_splay_add(Es, splay:add(E, Splay)).
+
+test_splay_remove([], Splay) -> Splay;
+test_splay_remove([_E|Es], Splay) ->
+    {S, _} = splay:delete_min(Splay),
+    test_splay_remove(Es, S).
+
+show_splay_height(Splay, N) ->
+    io:format("With ~p els, height should be lg(~p)=~p. Actual: ~p~n",
+              [ N, N, round(math:log(N)), splay:height(Splay)]).
+
+test_splay_fetch([], S) -> {ok, S};
+test_splay_fetch([E|Es], Splay) ->
+    case splay:has(E, Splay) of
+        {true, S} -> test_splay_fetch(Es, S);
+        {false, _S} -> erlang:error({missing_element, E})
+    end.
+
+test_splay_random_fetch(_Els, Splay, 0) -> {ok, Splay};
+test_splay_random_fetch(Els, Splay, N) ->
+    E = lists:nth(random:uniform(length(Els)), Els),
+    case splay:has(E, Splay) of
+        {true, S} -> test_splay_random_fetch(Els, S, N-1);
+        {false, _S} -> erlang:error({missing_element, E})
+    end.
+
 random_els(N) -> [ random:uniform() || _ <- lists:seq(1, N) ].
 random_kvs(N) -> [ {random:uniform(),I} || I <- lists:seq(1, N) ].
